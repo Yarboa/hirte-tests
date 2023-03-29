e3b38a723cd9528b4b296e059696c2b195a792da
Martin Perina <mperina@redhat.com>
2023-03-29 21:04:20 +0200

    Fix port mapping for hirte-mgr container
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- scripts/tests-setup.sh
+++ scripts/tests-setup.sh
@@ -3,7 +3,7 @@
 podman build -f ./containers/$CONTAINER_USED -t hirte-image .
 
 podman network create --subnet $TEST_NET_RANGE hirte-test
-podman run -d --net hirte-test --rm -p $MGR_ADD:$MGR_ADD --name hirte-mgr \
+podman run -d --net hirte-test --rm -p $MGR_PORT:$MGR_PORT --name hirte-mgr \
           $(podman images *hirte-image --format {{.ID}})
 podman run -d --net hirte-test --rm --name hirte-node-bar \
           $(podman images *hirte-image --format {{.ID}})
